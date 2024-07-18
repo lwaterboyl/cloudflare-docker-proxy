e36bd90fe20804a01b6ce0c683bed8c48668fb9f
kk <429079935@qq.com>
2024-07-18 20:27:33 +0800

    modify routers

--- src/index.js
+++ src/index.js
@@ -5,20 +5,6 @@ addEventListener("fetch", (event) => {
 
 const dockerHub = "https://registry-1.docker.io";
 
-// const routes = {
-//   // production
-//   "docker.libcuda.so": dockerHub,
-//   "quay.libcuda.so": "https://quay.io",
-//   "gcr.libcuda.so": "https://gcr.io",
-//   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-//   "k8s.libcuda.so": "https://registry.k8s.io",
-//   "ghcr.libcuda.so": "https://ghcr.io",
-//   "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-//   "ecr.libcuda.so": "https://public.ecr.aws",
-
-//   // staging
-//   "docker-staging.libcuda.so": dockerHub,
-// };
 
 const routes = {
     "docker.kklib.cloud": "https://registry-1.docker.io",
@@ -28,6 +14,7 @@ const routes = {
     "k8s.kklib.cloud": "https://registry.k8s.io",
     "ghcr.kklib.cloud": "https://ghcr.io",
     "cloudsmith.kklib.cloud": "https://docker.cloudsmith.io",
+    "ecr.kklib.cloud": "https://public.ecr.aws",
   };
 
 function routeByHosts(host) {
